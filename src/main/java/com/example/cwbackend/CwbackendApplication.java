bab48099f0c4de7fd73c43249ff6ac755b004b68
Lidiia <lidochkapop@outlook.com>
2022-12-21 14:55:54 +0200

    Initial commit

---
+++ src/main/java/com/example/cwbackend/CwbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cwbackend/CwbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
